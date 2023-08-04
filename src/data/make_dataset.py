fe55f7a90110f2124100d1f325899700a7e9663c
Adrian Mohnacs <amohnacs@gmail.com>
2023-08-04 15:44:19 +0700

    Comparison v2. Better code and more complete comparison.
    Save our graphs programmatically.
    Loops to get all the information we need using custom sized DF.
    Calculate the complete average based on columns.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,14 +9,13 @@ print(f"Total number of files: {len(all_files)}")
 
 accuweather_df = pd.DataFrame()
 accuweather_meteo_df = pd.DataFrame()
-metea_model_df = pd.DataFrame()
+meteo_model_df = pd.DataFrame()
 
 sensor1_df = pd.DataFrame()
 sensor2_df = pd.DataFrame()
 
 for filename in all_files:
   curr_filename = filename.replace(' ', '_').replace('. ', '_')
-  print(filename)
 
   with open(filename, 'r', encoding='utf-8', errors='ignore') as file:
     if 'accuweather_hourly' in filename:
@@ -24,7 +23,7 @@ for filename in all_files:
     elif 'meteo_data_for_accuweather_comparison' in filename:
       accuweather_meteo_df = pd.read_csv(file)
     elif 'meteo_data_for_model' in filename:
-      metea_model_df = pd.read_csv(file)  
+      meteo_model_df = pd.read_csv(file)  
     elif 'Sensor1' in filename:
       sensor1_df = pd.read_csv(file)
     elif 'Sensor2' in filename:
@@ -35,9 +34,6 @@ for filename in all_files:
 # ---------------------------------------------------------------
 # Validate Accuweather meets our 5% margin of error for accuracy
 # ---------------------------------------------------------------
-
-print(f"Accuweather: { accuweather_df.columns }")
-print(f"Accuweather Meteo Baseline: { accuweather_meteo_df.columns }")
 
 # Column cleaning. Strip white spaces
 accuweather_df.columns = accuweather_df.columns.str.strip()
@@ -66,31 +62,69 @@ accuweather_meteo_df.index = pd.to_datetime(accuweather_meteo_df.index)
 #
 # Understand the Alignment of our Data Sources
 #
+accuweather_comp_columns = ['Temperature', 'Humidity (%)', 'DewPoint', 'Wind Speed (km/h)', 'WindGust  (km/h)', 'UVIndex', 'WetBulb', 'Pressure (mb)']
+accuweather_meteo_comp_columns = ['Temp - C', 'Hum - %', 'Dew Point - C', 'Wind Speed - km/h', 'High Wind Speed - km/h', 'UV Index', 'Wet Bulb - C', 'Barometer - hPa']
+# Use zip() to create pairs and dict() to convert these pairs to a dictionary
+comp_dict =  dict(zip(accuweather_comp_columns, accuweather_meteo_comp_columns))
 
 # start visually. TODO different graphs and drop irrelevant columns
-accuweather_hourly = accuweather_df.resample('H').mean()
-accuweather_meteo_hourly = accuweather_meteo_df.resample('H').mean()
+accuweather_comp_hourly = accuweather_df[accuweather_comp_columns].resample('H').mean()
+accuweather_meteo_comp_hourly = accuweather_meteo_df[accuweather_meteo_comp_columns].resample('H').mean()
 
 # Replace all irregular values and NaN to smooth out dataset
-accuweather_hourly.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
-accuweather_meteo_hourly.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+accuweather_comp_hourly.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+accuweather_meteo_comp_hourly.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
 
-# TODO for loop for our specific column values
-plt.figure(figsize=(20,10))
-accuweather_hourly.head(400)['Pressure (mb)'].plot(label='Accuweather')
-accuweather_meteo_hourly.head(400)['Barometer - hPa'].plot(label='Weather Station')
-plt.xlabel('Date & Time')
-plt.ylabel('Pressure')
-plt.title('Data Source Comparison')
-plt.legend()
+# loop through to save to file and then show the graph
+for accuweather_col, accuweather_meteo_col in comp_dict.items():
+  plt.figure(figsize=(20,10))
+  accuweather_comp_hourly.head(400)[accuweather_col].plot(label='Accuweather')
+  accuweather_meteo_comp_hourly.head(400)[accuweather_meteo_col].plot(label='Weather Station')
+  plt.xlabel('Date & Time')
+  plt.ylabel(accuweather_col)
+  plt.title('Data Source Comparison')
+  plt.legend()
+
+  plt.savefig(f"../../reports/figures/accuweather_weather_station_comparison_{accuweather_col.replace('/', '')}.png")
+
+  plt.show()
+
+# calculate the percentage difference between the two data sources by feature
+calculate_percent_difference_df = pd.DataFrame()
+threshold_percentage_df = pd.DataFrame()
+
+for accuweather_col, accuweather_meteo_col in comp_dict.items():
+  difference_col = f"{accuweather_col} Percent Difference"
+  threshold_column = f"{accuweather_col} Within Threshold"
+
+  calculate_percent_difference_df[difference_col] = abs(accuweather_comp_hourly[accuweather_col].sub(accuweather_meteo_comp_hourly[accuweather_meteo_col]) / accuweather_comp_hourly[accuweather_col]) * 100
+  calculate_percent_difference_df[difference_col] = calculate_percent_difference_df[difference_col].round(2)
+  calculate_percent_difference_df.dropna(inplace=True)
+
+  calculate_percent_difference_df[threshold_column] = calculate_percent_difference_df[difference_col] <= 5
+  percentage_within_threshold = (calculate_percent_difference_df[threshold_column].mean() * 100).round(2)
+
+  threshold_percentage_df[threshold_column] = [percentage_within_threshold]
+
+print(threshold_percentage_df)
+# -----------------------------------------------------------------
+# Corrolation Research To Find The Best Features For Model Training
+# -----------------------------------------------------------------
+
+#
+# Get Our Whole View Of Corrolations
+#
+
+# Calculating the correlation matrix for all columns
+correlation_matrix = accuweather_df.corr(method='pearson')
+
+# Plotting a heatmap for visualization
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
+plt.title("Correlation Heatmap")
 plt.show()
 
-# let's look at the quantitative difference
-percent_difference = pd.DataFrame()
-percent_difference['Percent Difference'] = abs(accuweather_hourly['Temperature'].sub(accuweather_meteo_hourly['Temp - C']) / accuweather_hourly['Temperature']) * 100
-percent_difference['Percent Difference'] = percent_difference['Percent Difference'].round(2)
-percent_difference.dropna(inplace=True)
-percent_difference['Within Threshold'] = percent_difference['Percent Difference'] <= 5
-percentage_within_threshold = (percent_difference['Within Threshold'].mean() * 100).round(2)
+# Returning the correlation values with the target variable 'clAndijk'
+correlation_with_clAndijk = correlation_matrix['clAndijk']
+correlation_with_clAndijk.sort_values(ascending=False)
 
-print("Percentage of data within 5% threshold: ", percentage_within_threshold)
