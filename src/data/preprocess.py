8afc4328387154cf5c046bf0e120103413384bb2
Adrian Mohnacs <amohnacs@gmail.com>
2023-08-29 14:34:56 +0700

    Outlier preprocessing to SKlearn Pipeline

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -39,7 +39,7 @@ class FileLoader(BaseEstimator, TransformerMixin):
 
         return df
     
-class ColumnCleaner(BaseEstimator, TransformerMixin):
+class ColumnSpaceCleaner(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
@@ -47,8 +47,7 @@ class ColumnCleaner(BaseEstimator, TransformerMixin):
         X.columns = X.columns.str.strip()
         return X
 
-
-class SimpleDateTimeIndexSetter(BaseEstimator, TransformerMixin):
+class DateTimeIndexSetter(BaseEstimator, TransformerMixin):
     def __init__(self, datetime_column):
         self.datetime_column = datetime_column
 
@@ -69,111 +68,160 @@ class ToPickleSaver(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         X.to_pickle(self.pickle_name)
         return X
-    
-accuweather_pipeline = Pipeline([
-    ('file_loader', FileLoader('../../data/raw/accuweather_hourly_1.29_to_6.15.csv')),
-    ('column_cleaner', ColumnCleaner()),
-    ('datetime_index_setter', SimpleDateTimeIndexSetter('Date & Time')),
-    ('pickle_saver', ToPickleSaver('../../data/interim/01_accuweather_comparison_datetime_df.pkl'))
-])  
-
-meteo_data_pipeline = Pipeline([
-    ('file_loader', FileLoader('../../data/raw/meteo_data_for_accuweather_comparison_1.30_to_6.15.csv')),
-    ('column_cleaner', ColumnCleaner()),
-    ('datetime_index_setter', SimpleDateTimeIndexSetter('Date & Time')),
-    ('pickle_saver', ToPickleSaver('../../data/interim/01_accuweather_metero_comparison_datetime_df.pkl'))
-])  
-
-accuweather_df = accuweather_pipeline.fit_transform(X=None)
-accuweather_meteo_df= meteo_data_pipeline.fit_transform(X=None)
-
-correlation_pipeline = Pipeline([])
-
-# --------------------------------------------------------------
-# Prepare our comparison dataframes
-# --------------------------------------------------------------
-
-accuweather_df, accuweather_meteo_df = load_comparative_analysis_data()
-
-
-# Set date column to proper datetime format
-accuweather_df = set_datetime_as_index(accuweather_df, 'Date & Time')
-accuweather_meteo_df = set_datetime_as_index(accuweather_meteo_df, 'Date & Time')
-
-# Save to pickle file
-accuweather_df.to_pickle('../../data/interim/01_accuweather_comparison_datetime_df.pkl')
-accuweather_meteo_df.to_pickle('../../data/interim/01_accuweather_metero_comparison_datetime_df.pkl')
 
 # --------------------------------------------------------------
 # Prepare our Correlation dataframes
 # --------------------------------------------------------------
 
-meteo_model_df, sensor1_df, sensor2_df = load_correlation_analysis_data()
+class ColumnNameCleaner(BaseEstimator, TransformerMixin):
+    def __init__(self, old_col_name, new_col_name):
+        self.old_col_name = old_col_name
+        self.new_col_name = new_col_name
 
-sensor2_df.rename(columns={'Date&Time': 'Date & Time'}, inplace=True)
+    def fit(self, X, y=None):
+        return self
 
-# set dates as index
-meteo_model_df = set_datetime_as_index(meteo_model_df, 'Date & Time')
-sensor1_df = set_datetime_as_index(sensor1_df, 'Date & Time')
-sensor2_df = set_datetime_as_index(sensor2_df, 'Date & Time')
+    def transform(self, X, y=None):
+        X.rename(columns={ self.old_col_name: self.new_col_name }, inplace=True)
+        return X
+class SensorGroupBy(BaseEstimator, TransformerMixin):
+    def __init__(self, group_by_column):
+        self.group_by_column = group_by_column
 
-sensor1_df = sensor1_df.groupby('Date & Time').mean()
-sensor2_df = sensor2_df.groupby('Date & Time').mean()
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.groupby(self.group_by_column).mean()
+        return X
+    
+class Resampler(BaseEstimator, TransformerMixin):
+    def __init__(self, resample_interval):
+        self.resample_interval = resample_interval
 
-# resampling to 15 min intervals. setting the average within each 15 min bucket
-# we could use reset_index() to get the date & time back as a column
-meteo_model_resampled = meteo_model_df.resample('15T').mean()
-sensor1_resampled = sensor1_df.resample('15T').mean()
-sensor2_resampled = sensor2_df.resample('15T').mean()
-
-# Merge the two sensor dataframes then merge that with weather data
-sensor_merged = pd.merge(sensor1_resampled, sensor2_resampled, left_index=True, right_index=True)
-complete_meteo_sensor_df = pd.merge(meteo_model_resampled, sensor_merged, left_index=True, right_index=True, how='left')
-
-# remove the null values
-complete_meteo_sensor_df.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        X = X.resample(self.resample_interval).mean()
+        return X
 
 #--------------------------------------------------------------
-# Remove The highest values and set moisture
+# Remove The highest values and remove outliers
 #--------------------------------------------------------------
 
-pre_clean_df = complete_meteo_sensor_df.copy()
+class ExtremeValueRemover(BaseEstimator, TransformerMixin):
+    def __init__(self, column_name, max_value):
+        self.column_name = column_name
+        self.max_value = max_value
 
-# Remove the highest values
-moist_meteo_sensor_df = pre_clean_df[pre_clean_df['Sensor1 (Ohms)'] < 50000]
-moist_meteo_sensor_df = moist_meteo_sensor_df[moist_meteo_sensor_df['Sensor2 (Ohms)'] < 50000]
+    def fit(self, X, y=None):
+        return self
 
-# --------------------------------------------------------------
-# Removing Outliers
-# --------------------------------------------------------------
+    def transform(self, X, y=None):
+        X = X[X[self.column_name] < self.max_value]
+        return X
 
-outlier_columns = list(moist_meteo_sensor_df.columns)
+class RemoveOutliersWithLOF(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
 
-print('IQR method')
-for col in outlier_columns:
-  iqr_outlier_plot_dataset = remove_outliers.mark_outliers_iqr(moist_meteo_sensor_df, col)
-  remove_outliers.plot_binary_outliers(iqr_outlier_plot_dataset, col, outlier_col=col + '_outlier', reset_index=True)
+    def fit(self, X, y=None):
+        return self
 
-print('Chauvenet method')
-for col in outlier_columns:
-  chauvenet_outlier_plot_dataset = remove_outliers.mark_outliers_chauvenet(moist_meteo_sensor_df, col)
-  remove_outliers.plot_binary_outliers(chauvenet_outlier_plot_dataset, col, outlier_col=col + '_outlier', reset_index=True)
+    def transform(self, X, y=None):
 
-print('LOF method')
-lof_outlier_plot_dataset, outliers, X_scores = remove_outliers.mark_outliers_lof(moist_meteo_sensor_df, outlier_columns)
-for col in outlier_columns:
-  remove_outliers.plot_binary_outliers(dataset=lof_outlier_plot_dataset, col=col, outlier_col="outlier_lof", reset_index=True)
+      outlier_safe_df, outliers, X_scores = remove_outliers.mark_outliers_lof(X, self.columns)
+      for column in self.columns:
+        outlier_safe_df.loc[outlier_safe_df['outlier_lof'], column] = np.nan
 
-# We will choose LOF and remove the outliers
-real_outlier_columns = ['Sensor1 (Ohms)', 'Sensor2 (Ohms)']
+      # remove the null values
+      outlier_safe_df.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+      outlier_safe_df.drop('outlier_lof', axis=1, inplace=True)
 
-outlier_safe_df, outliers, X_scores = remove_outliers.mark_outliers_lof(moist_meteo_sensor_df, real_outlier_columns)
-for column in real_outlier_columns:
-  outlier_safe_df.loc[outlier_safe_df['outlier_lof'], column] = np.nan
+      return outlier_safe_df    
 
-# remove the null values
-outlier_safe_df.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
-outlier_safe_df.drop('outlier_lof', axis=1, inplace=True)
+def plot_outliers(source_df):
+  moist_meteo_sensor_df = source_df.copy()
+  outlier_columns = list(moist_meteo_sensor_df.columns)
 
-outlier_safe_df.to_pickle('../../data/interim/02_outlier_safe_complete_datetime_df.pkl')
+  print('IQR method')
+  for col in outlier_columns:
+    iqr_outlier_plot_dataset = remove_outliers.mark_outliers_iqr(moist_meteo_sensor_df, col)
+    remove_outliers.plot_binary_outliers(iqr_outlier_plot_dataset, col, outlier_col=col + '_outlier', reset_index=True)
+
+  print('Chauvenet method')
+  for col in outlier_columns:
+    chauvenet_outlier_plot_dataset = remove_outliers.mark_outliers_chauvenet(moist_meteo_sensor_df, col)
+    remove_outliers.plot_binary_outliers(chauvenet_outlier_plot_dataset, col, outlier_col=col + '_outlier', reset_index=True)
+
+  print('LOF method')
+  lof_outlier_plot_dataset, outliers, X_scores = remove_outliers.mark_outliers_lof(moist_meteo_sensor_df, outlier_columns)
+  for col in outlier_columns:
+    remove_outliers.plot_binary_outliers(dataset=lof_outlier_plot_dataset, col=col, outlier_col="outlier_lof", reset_index=True)
+
+
+def preprocess():
+      
+  accuweather_pipeline = Pipeline([
+      ('file_loader', FileLoader('../../data/raw/accuweather_hourly_1.29_to_6.15.csv')),
+      ('column_cleaner', ColumnSpaceCleaner()),
+      ('datetime_index_setter', DateTimeIndexSetter('Date & Time')),
+      ('pickle_saver', ToPickleSaver('../../data/interim/01_accuweather_comparison_datetime_df.pkl'))
+  ])  
+
+  meteo_data_pipeline = Pipeline([
+      ('file_loader', FileLoader('../../data/raw/meteo_data_for_accuweather_comparison_1.30_to_6.15.csv')),
+      ('column_cleaner', ColumnSpaceCleaner()),
+      ('datetime_index_setter', DateTimeIndexSetter('Date & Time')),
+      ('pickle_saver', ToPickleSaver('../../data/interim/01_accuweather_metero_comparison_datetime_df.pkl'))
+  ])  
+
+  accuweather_df = accuweather_pipeline.fit_transform(X=None)
+  accuweather_meteo_df= meteo_data_pipeline.fit_transform(X=None)
+
+  meteo_model_pipeline = Pipeline([
+      ('file_loader', FileLoader('../../data/raw/meteo_data for model 30.1.2023. - 31.7.2023..csv')),
+      ('datetime_index_setter', DateTimeIndexSetter('Date & Time')),
+      ('resampler', Resampler('15T')),
+      ('pickle_saver', ToPickleSaver('../../data/interim/01_accuweather_comparison_datetime_df.pkl'))
+  ])  
+
+  sensor1_pipeline = Pipeline([
+      ('file_loader', FileLoader('../../data/raw/Sensor1 data 21.1.2023..csv')),
+      ('datetime_index_setter', DateTimeIndexSetter('Date & Time')),
+      ('resampler', Resampler('15T')),
+  ]) 
+
+  sensor2_pipeline = Pipeline([
+      ('file_loader', FileLoader('../../data/raw/Sensor2 data 21.1.2023..csv')),
+      ('datetime_name_cleaner', ColumnNameCleaner('Date&Time', 'Date & Time')),
+      ('datetime_index_setter', DateTimeIndexSetter('Date & Time')),
+      ('resampler', Resampler('15T')),
+  ]) 
+
+  meteo_model_df = meteo_model_pipeline.fit_transform(X=None)
+  sensor1_df= sensor1_pipeline.fit_transform(X=None)        
+  sensor2_df= sensor2_pipeline.fit_transform(X=None)     
+
+  # Merge the two sensor dataframes then merge that with weather data
+  sensor_merged = pd.merge(sensor1_df, sensor2_df, left_index=True, right_index=True)
+  complete_meteo_sensor_df = pd.merge(meteo_model_df, sensor_merged, left_index=True, right_index=True, how='left')
+
+  # remove the null values
+  complete_meteo_sensor_df.interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
+
+  # plot_outliers(complete_meteo_sensor_df)
+  
+  outlier_pipeline = Pipeline([
+      ('sensor1_extreme_value_remover', ExtremeValueRemover('Sensor1 (Ohms)', 50000)),
+      ('sensor2_extreme_value_remover', ExtremeValueRemover('Sensor2 (Ohms)', 50000)),
+      ('remove_outliers_with_lof', RemoveOutliersWithLOF(['Sensor1 (Ohms)', 'Sensor2 (Ohms)'])),
+      ('pickle_saver', ToPickleSaver('../../data/interim/02_outlier_safe_complete_datetime_df.pkl'))
+  ])
+
+  outlier_pipeline.fit_transform(X=complete_meteo_sensor_df)
+
+if __name__ == "__main__":
+    preprocess()
 
